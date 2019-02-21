90d0f7f75d7b2b1f110282563d5e8241d2241d60
acharyakavita <acharyakavita12@gmail.com>
2019-02-21 14:53:54 -0500

    added mongoose

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
 const express=require('express');
-let app=express();
-let bodyparser=require('body-parser');
+const app=express();
+const bodyparser=require('body-parser');
+const mongoose=require('mongoose');
 
+mongoose.connect('mongodb://localhost/yelp_camp', { useNewUrlParser: true });
 
 app.use(bodyparser.urlencoded({extended:true}))
-let campground=[{name:'salmon-creek', image:'https://cdn.pixabay.com/photo/2016/02/18/22/16/tent-1208201_1280.jpg'},
-{name:'Granite Hill',image:'https://cdn.pixabay.com/photo/2016/11/29/04/17/bonfire-1867275_1280.jpg'},{name:'salmon-creek', image:'https://cdn.pixabay.com/photo/2016/02/18/22/16/tent-1208201_1280.jpg'},
-{name:'Granite Hill',image:'https://cdn.pixabay.com/photo/2016/11/29/04/17/bonfire-1867275_1280.jpg'},
-{name:'salmon-creek', image:'https://cdn.pixabay.com/photo/2016/02/18/22/16/tent-1208201_1280.jpg'},
-{name:'Granite Hill',image:'https://cdn.pixabay.com/photo/2016/11/29/04/17/bonfire-1867275_1280.jpg'},{name:'salmon-creek', image:'https://cdn.pixabay.com/photo/2016/02/18/22/16/tent-1208201_1280.jpg'},
-{name:'Granite Hill',image:'https://cdn.pixabay.com/photo/2016/11/29/04/17/bonfire-1867275_1280.jpg'},
-{name:'salmon-creek', image:'https://cdn.pixabay.com/photo/2016/02/18/22/16/tent-1208201_1280.jpg'},
-{name:'Granite Hill',image:'https://cdn.pixabay.com/photo/2016/11/29/04/17/bonfire-1867275_1280.jpg'},
-{name:'salmon-creek', image:'https://cdn.pixabay.com/photo/2016/02/18/22/16/tent-1208201_1280.jpg'},
-{name:'Granite Hill',image:'https://cdn.pixabay.com/photo/2016/11/29/04/17/bonfire-1867275_1280.jpg'}]
+
+//schema setup
+const campgroundSchema= new mongoose.Schema({
+    name:String,
+    image:String
+})
+
+const Campground=mongoose.model('Campground',campgroundSchema);
+
 
 app.set('view engine','ejs')
 
@@ -22,15 +23,32 @@ app.get('/',function(req,res){
 })
 
 app.get('/campgrounds',function(req,res){
-    res.render('campgrounds',{campground:campground})
+    Campground.find({},function(err,allCamps){
+        if(err){
+            console.log(err)
+        }
+        else
+        {
+            res.render('campgrounds',{campground:allCamps})
+        }
+    })
+    
 })
 
 app.post('/campgrounds',function(req,res){
     let newCampground=new Object();
     newCampground.name= req.body.name
     newCampground.image=req.body.url
-    campground.push(newCampground)
-    res.redirect('/campgrounds')
+    Campground.create(newCampground,function(err,newCamp){
+        if(err){
+            console.log(err)
+        }
+        else
+        {
+            res.redirect('/campgrounds')
+    }
+    })
+    
 })
 
 app.get('/campgrounds/new',function(req,res){
