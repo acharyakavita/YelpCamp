c3a15a9163e0d867d61eb521542ae47e6f5fc216
acharyakavita <acharyakavita12@gmail.com>
2019-03-18 18:26:44 -0400

    added Comments page

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.get('/campgrounds',function(req,res){
         }
         else
         {
-            res.render('index',{campground:allCamps})
+            res.render('campgrounds/index',{campground:allCamps})
         }
     })
     
@@ -48,21 +48,55 @@ app.post('/campgrounds',function(req,res){
 })
 
 app.get('/campgrounds/new',function(req,res){
-
-    res.render('new')
+    res.render('campgrounds/new')
 })
 
 app.get('/campgrounds/:id',function(req,res){
-    Campground.findById(req.params.id,function(err,foundCamp){
+    Campground.findById(req.params.id).populate("comments").exec(function(err,foundCamp){
         if(err){
             console.log(err)
         }
         else{
-            res.render('show',{campground:foundCamp})
+            res.render('campgrounds/show',{campground:foundCamp})
         }
-    })
+    });
 })
 
+// comments routes
+
+app.get('/campgrounds/:id/comments/new',function(req,res){
+    Campground.findById(req.params.id,function(err,campground){
+        if(err){
+            console.log(err)
+        }
+        else{
+            res.render('comments/new',{campground:campground})
+        }
+    })
+    
+})
+
+app.post('/campgrounds/:id/comments',function(req,res){
+    Campground.findById(req.params.id,function(err,campground){
+        if(err){
+            console.log(err)
+            res.redirect('/campgrounds')
+        }
+        else{
+            Comment.create(req.body.comment,function(err,comment){
+                if(err){
+                    console.log(err)
+                }
+                else{
+                    campground.comments.push(comment);
+                    campground.save()
+                    res.redirect('/campgrounds/'+campground._id)
+                }
+            })
+        }
+    })
+    
+})
 app.listen('3000',function(){
     console.log('server is running')
 })
