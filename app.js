be05cc8778702131f1df5f354b20e687caf059e2
acharyakavita <acharyakavita12@gmail.com>
2019-03-19 17:32:20 -0400

    modified navbar

--- app.js
+++ app.js
@@ -30,6 +30,12 @@ passport.use(new LocalStrategy(User.authenticate()))
 passport.serializeUser(User.serializeUser())
 passport.deserializeUser(User.deserializeUser())
 
+//middleware to passe the user name in every route
+app.use(function(req,res,next){
+    res.locals.currentUser=req.user
+    next();
+})
+
 app.get('/',function(req,res){
     res.render('landing')
 })
