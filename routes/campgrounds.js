9b3f5cede27e4785b8299c155222189a920f0111
acharyakavita <acharyakavita12@gmail.com>
2019-03-20 13:16:19 -0400

    added campground authorization

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -58,7 +58,7 @@ router.get('/:id',function(req,res){
 
 
 //edit and update
-router.get('/:id/edit',function(req,res){
+router.get('/:id/edit',checkCampgroundOwnership,function(req,res){
     Campground.findById(req.params.id,function(err,foundCamp){
         if(err){
             console.log(err)
@@ -86,7 +86,7 @@ router.put('/:id',function(req,res){
 })
 
 //destroy
-router.delete('/:id',function(req,res){
+router.delete('/:id',checkCampgroundOwnership,function(req,res){
     Campground.findByIdAndRemove(req.params.id,function(err){
         if(err){
             console.log(err)
@@ -98,6 +98,25 @@ router.delete('/:id',function(req,res){
     })
     
 })
+
+function checkCampgroundOwnership(req,res,next){
+    if(req.isAuthenticated()){
+        Campground.findById(req.params.id, function(err, foundCampground){
+           if(err){
+               res.redirect("back");
+           }  else {
+               // does user own the campground?
+            if(foundCampground.author.id.equals(req.user._id)) {
+                next();
+            } else {
+                res.redirect("back");
+            }
+           }
+        });
+    } else {
+        res.redirect("back");
+    }
+}
 
 function isLoggedIn(req,res,next){
     if(req.isAuthenticated()){
