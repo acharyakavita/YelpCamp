6be14b662937c77e7d14935b34f944cb498d953f
acharyakavita <acharyakavita12@gmail.com>
2019-03-21 11:49:29 -0400

    added user profile and password reset functionality'

--- models/user.js
+++ models/user.js
@@ -2,9 +2,15 @@ var mongoose = require("mongoose");
 const passportLocalMongoose=require('passport-local-mongoose');
  
 var userSchema = new mongoose.Schema({
-    username: String,
+    username: {type:String,unique:true,required:true},
     password: String,
-    isAdmin:{type:Boolean,default:false}
+    isAdmin:{type:Boolean,default:false},
+    avatar:String,
+    firstName:String,
+    lastName:String,
+    email:{type:String,unique:true,required:true},
+    resetPasswordToken:String,
+    resetPasswordExpires:Date
 });
  
 userSchema.plugin(passportLocalMongoose);
