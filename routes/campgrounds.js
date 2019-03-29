e86b1dbab8751cba4cb656dd3837b13c78fd0109
acharyakavita <acharyakavita12@gmail.com>
2019-03-29 12:59:33 -0400

    bug fix

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -1,6 +1,7 @@
 const express=require('express')
 const router=express.Router()
 const Campground=require('../models/campground');
+const Comment=require('../models/comment');
 const User=require('../models/user');
 const middleware=require('../middleware/index');
 var Notification = require("../models/notification");
